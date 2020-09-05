25d48306bc9b6a9d536ca8190b53b6d08d64f0a8
Ayrton <ayrtonmuniz2002@gmail.com>
2020-09-04 19:13:45 -0700

    Moved files

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static GameObject player = default;
+
+    void Awake()
+    {
+        player = this.gameObject;   
+    }
+}
