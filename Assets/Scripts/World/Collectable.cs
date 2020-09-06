c20a910e04a81fb231adc4d219a8b6670c296e79
Ayrton <ayrtonmuniz2002@gmail.com>
2020-09-06 03:59:08 -0700

    EndGame and More Gameplay
    Gameplay on the main end island revolving around spikes and added end game music when reaching home

---
+++ Assets/Scripts/World/Collectable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Collectable : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+       
+        if (other.GetComponent<CheckpointManager>() != null)
+        {
+            Player.player.GetComponent<AudioManager>().Play("Fly");
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/World/EndGame.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.GetComponent<CheckpointManager>() != null)
+        {
+            GameObject.FindGameObjectWithTag("GameController").GetComponent<AudioManager>().Stop("Music");
+            GameObject.FindGameObjectWithTag("GameController").GetComponent<AudioManager>().Play("EndGame");
+            
+        }
+    }
+}
