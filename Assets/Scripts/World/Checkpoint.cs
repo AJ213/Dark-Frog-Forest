c6889e97333f3df7b7bba9f255bc4442eb5f2274
Ayrton <ayrtonmuniz2002@gmail.com>
2020-09-05 23:54:49 -0700

    Added checkpoint sound

--- Assets/Scripts/World/Checkpoint.cs
+++ Assets/Scripts/World/Checkpoint.cs
@@ -8,12 +8,14 @@ public class Checkpoint : MonoBehaviour
 {
     bool actived = false;
     [SerializeField] ParticleSystem particles = default;
+    [SerializeField] AudioSource checkpointRecieved = default;
     private void OnTriggerEnter(Collider other)
     {
         if (actived) return;
         if (other.GetComponent<CheckpointManager>() != null)
         {
             other.GetComponent<CheckpointManager>().lastCheckpoint = this.transform;
+            checkpointRecieved.Play();
             actived = true;
         }
     }
